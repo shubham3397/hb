0cbd26129368a7188e766cab25780878ed48d160
shubhangi <shubhangi@shubhangi-PC>
2016-06-26 06:13:29 -0700

    trying to create new

--- joining_git/src/joining_git/Joining_git.java
+++ joining_git/src/joining_git/Joining_git.java
@@ -16,5 +16,6 @@ public class Joining_git {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello git");
+                int a=27;
     }
 }
